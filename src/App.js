58097a07e19ca219489ca9945534383f5136afc0
Ruaridh MacDowall <ruaridh@seamlessml.com>
2020-12-12 18:14:38 +0000

    kinda working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import Tutorial from './tutorial.js';
+import './App.css';
 
 class Canvas extends React.Component {
-  componentDidMount() {
-    const canvas = this.refs.canvas;
-    const ctx = canvas.getContext("2d");
-    ctx.fillRect(50,50,2000,50);
-    var game = new Game();
-    game.iterate();
+  constructor() {
+    super()
+    this.state = {
+	  width: null,
+	  height: null,
+	  ctx: null,
+	  isRunning: true,
+    };
   }
+
+  componentDidMount() {
+    let canvas = this.refs.canvas;
+    let ctx = canvas.getContext("2d");
+    ctx.canvas.width = window.innerWidth;
+	ctx.canvas.height = window.innerHeight;
+    var game = new Game();
+    this.setState({width: canvas.width});
+    this.setState({height: canvas.height});
+    setInterval(()=>{
+	if (this.state.isRunning) {
+		let cells = game.iterate();
+		this.draw(cells, ctx)
+	}
+     }, 250)
+     }
+
+  updateRunning = () => {
+    this.setState({isRunning: !this.state.isRunning})
+  }
+
+  draw(cells, ctx){
+    ctx.fillStyle="#E0E0E0";
+    ctx.fillRect(0,0,this.state.width,this.state.height);
+    ctx.fillStyle="#000000";
+    cells.forEach((row,y,grid)=>{
+      row.forEach((cell_state,x,row)=>{
+	ctx.fillRect(y*10+31 , x*10+31  ,8,8);
+      })
+    })
+  }
+
   render (){
     return (
       <div className="App">
@@ -16,7 +51,11 @@ class Canvas extends React.Component {
         </header>
         <body>
 	<Tutorial/>
-        <canvas ref="canvas" width="200" height="100"></canvas>
+	<button className="button" onClick={this.updateRunning}>
+	    {this.state.isRunning ? <div>Stop</div>
+                            : <div>Start</div>}
+	</button>
+        <canvas ref="canvas"></canvas>
         </body>
       </div>
     );
@@ -30,55 +69,77 @@ class Game {
   constructor() {
     this.state = {
       grid: this.getInitialGrid(),
-      interval: 100,
-      isRunning: false,
-      drawer: Canvas,
     };
   }
 
   iterate(){
+    console.log(this.state.grid)
     this.updateGrid();
-    //this.drawer.draw();
+    return this.state.grid;
   }
 
   updateGrid(){
+    let counts = this.getCounts()
+    let newGrid = this.generateNewGrid(counts)
+    this.state.grid = newGrid
+  }
+
+  getCounts() {
     let counts = new Map();
-    let buffer = new Map(this.state.grid);
     this.state.grid.forEach((row,y,grid)=>{
       row.forEach((cell_state,x,row)=>{
 	for(let i=-1; i<2; i++){
 	   for(let j =-1; j<2; j++){
-		if(i===0 && j===0){continue;}
-		if(!counts.get(y+i)){
+		if(!counts.has(y+i)){
 		   counts.set(y+i, new Map())
 		}
-		if(!counts.get(y+i).get(x+j)){
+		if(!counts.get(y+i).has(x+j)){
 		   counts.get(y+i).set(x+j, 0);
 		}
-		   counts.get(y+i).set(x+j, counts.get(y+i).get(x+j)+1);
+		counts.get(y+i).set(x+j, counts.get(y+i).get(x+j)+1);
 	   }
 	}
-      console.log('grid', grid)
-	console.log('counts', counts)
-        if(buffer.get(y-1)){
-          buffer.get(y-1).get(x-1);
-        }
-        else{
-          console.log("meh");
-        }
-      }
-      )
-    }
-  )}
+    })
+    })
+    return counts;
+  }
+
+  generateNewGrid(counts){
+     let buffer = new Map();
+     counts.forEach((row, y, counts)=>{
+	row.forEach((count, x, row)=>{
+		if(count===3){
+		   if(!buffer.has(y)){
+			   buffer.set(y, new Map());
+		   }
+		   buffer.get(y).set(x,0);
+		}
+		if(count===4){
+		  if(this.state.grid.get(y).has(x)){
+		     if(!buffer.has(y)){
+			   buffer.set(y, new Map());
+		     }
+		     buffer.get(y).set(x,0);
+		  }
+		}
+      })
+     })
+     return buffer;
+  }
 
   getInitialGrid() {
     const grid = new Map();
-      for(let i=0; i<2; i++){
-        let row= new Map();
+	let row= new Map();
         row.set(1,0);
-        row.set(2,0);
-        grid.set(i,row);
-      }
+	grid.set(1,row);
+	row = new Map();
+	row.set(2,0)
+	grid.set(2,row);
+	row = new Map();
+	row.set(0,0)
+	row.set(1,0)
+	row.set(2,0)
+	grid.set(3,row);
     return grid;
   }
 }
