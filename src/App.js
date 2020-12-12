8a8d44c5c44656023774c738eddc726cffaa9f18
Ruaridh MacDowall <ruaridh@seamlessml.com>
2020-12-12 16:50:14 +0000

    add counts

--- src/App.js
+++ src/App.js
@@ -27,48 +27,58 @@ export default Canvas;
 
 
 class Game {
-    constructor() {
-          this.state = {
-                  grid: this.getInitialGrid(),
-                  interval: 100,
-                  isRunning: false,
+  constructor() {
+    this.state = {
+      grid: this.getInitialGrid(),
+      interval: 100,
+      isRunning: false,
       drawer: Canvas,
-      };
-    }
+    };
+  }
 
-    iterate(){
-      this.updateGrid();
-      //this.drawer.draw();
-    }
+  iterate(){
+    this.updateGrid();
+    //this.drawer.draw();
+  }
 
-    updateGrid(){
-      let buffer = new Map();
-      this.state.grid.forEach((row,y,grid)=>{
-          row.forEach((cell_state,x,row)=>{
-          console.log(x, y, cell_state);
-            console.log(grid.get(y-1))
-            if(buffer.get(y-1)){
-              buffer.get(y-1).get(x-1);
-            }
-            else{
-              console.log("meh");
-            }
-
+  updateGrid(){
+    let counts = new Map();
+    let buffer = new Map(this.state.grid);
+    this.state.grid.forEach((row,y,grid)=>{
+      row.forEach((cell_state,x,row)=>{
+	for(let i=-1; i<2; i++){
+	   for(let j =-1; j<2; j++){
+		if(i===0 && j===0){continue;}
+		if(!counts.get(y+i)){
+		   counts.set(y+i, new Map())
+		}
+		if(!counts.get(y+i).get(x+j)){
+		   counts.get(y+i).set(x+j, 0);
+		}
+		   counts.get(y+i).set(x+j, counts.get(y+i).get(x+j)+1);
+	   }
+	}
+      console.log('grid', grid)
+	console.log('counts', counts)
+        if(buffer.get(y-1)){
+          buffer.get(y-1).get(x-1);
         }
-        )
+        else{
+          console.log("meh");
+        }
       }
-    )}
-
-
-
-    getInitialGrid() {
-  const grid = new Map();
-      for(let i=0; i<2; i++){
-      let row= new Map();
-      row.set(1,0);
-      row.set(2,0);
-      grid.set(i,row);
-      }
-  return grid;
+      )
     }
+  )}
+
+  getInitialGrid() {
+    const grid = new Map();
+      for(let i=0; i<2; i++){
+        let row= new Map();
+        row.set(1,0);
+        row.set(2,0);
+        grid.set(i,row);
+      }
+    return grid;
+  }
 }
