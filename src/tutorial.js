04434b5423f92687b7cfb6d84074e5d3e0869d81
Ruaridh MacDowall <ruaridhmacdowall@gmail.com>
2021-03-07 20:29:00 +0000

    updates

--- src/tutorial.js
+++ src/tutorial.js
@@ -27,6 +27,7 @@ Any live cell with fewer than two live neighbours dies, as if by underpopulation
 Any live cell with two or three live neighbours lives on to the next generation. <br/>
 Any live cell with more than three live neighbours dies, as if by overpopulation. <br/>
 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.<br/>
+Any new cell will become the colour of the majority of its neighbours.<br/>
 	    <br/>
         </div>
         <div className="actions">
